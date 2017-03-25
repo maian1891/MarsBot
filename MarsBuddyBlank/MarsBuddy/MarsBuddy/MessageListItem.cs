2a8da9566016f835826b992fb027555a06ca88ac
maian1891 <maian1891@gmail.com>
2017-03-25 11:29:08 +0700

    marbot

---
+++ MarsBuddyBlank/MarsBuddy/MarsBuddy/MessageListItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsBuddy
+{
+    class MessageListItem
+    {
+        public string Text { get; set; }
+        public string Sender { get; set; }
+        public MessageListItem(string text, string sender)
+        {
+            Text = text; Sender = sender;
+        }
+
+    }
+
+}
